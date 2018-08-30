e5e33802f54f10d36dd3effcb884780ab2a2b329
Taber McFarlin <tabermcfarlin@gmail.com>
2018-08-29 23:25:01 -0400

    Removes .js

--- lab1/routes/index.js
+++
@@ -1,8 +0,0 @@
-"use strict";
-exports.__esModule = true;
-function constructorMethod(app) {
-    app.use("*", function (req, res) {
-        res.status(404).json({ error: "Route not found." });
-    });
-}
-exports["default"] = constructorMethod;
